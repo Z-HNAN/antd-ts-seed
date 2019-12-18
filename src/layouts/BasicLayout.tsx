1238160085887e13a9821cc7cdbbff9ad769e1f0
Z-HNAN <zhn68033@163.com>
2019-12-18 23:27:29 +0800

    feat layout布局中做一层tabbar页面的判断

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -2,25 +2,30 @@
  * 普通布局
  */
 
-import React, { PureComponent } from 'react'
-import MenuBar from './MenuBar'
+import React from 'react'
 import withRouter from 'umi/withRouter'
-import { connect } from 'dva'
+import MenuBar, { tabBarData } from './MenuBar'
 
 import styles from './BasicLayout.less'
+
+// 需要路由跳转的页面
+const BarRoutes = tabBarData.map(({ link }) => link)
 
 const BasicLayout: React.FC<any> = props => {
   const { children, location } = props
 
+
+  // 不在tabBar中的
+  if (BarRoutes.indexOf(location.pathname) < 0) {
+    return <div className={styles.root}>{children}</div>;
+  }
+
   return (
     <div className={styles.root}>
-      <MenuBar pathname={location.pathname} >{children}</MenuBar>
+      <MenuBar pathname={location.pathname}>{children}</MenuBar>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {}
-}
 
-export default withRouter(connect(mapStateToProps)(BasicLayout))
+export default withRouter(BasicLayout)
