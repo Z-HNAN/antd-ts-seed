9b5ffd11fea683cc2a939712e7ef3fe8d3d79bb9
Z-HNAN <zhn68033@163.com>
2019-12-19 00:06:24 +0800

    feat 我的页面，并增加跳转功能

--- src/pages/me/index.tsx
+++ src/pages/me/index.tsx
@@ -3,5 +3,21 @@
  */
 
 import React from 'react'
+import { Button } from 'antd-mobile'
+import Router from 'umi/router';
 
-export default () => <div>我的</div>
+const Me: React.FC<{}> = () => {
+
+  const handleClick = () => {
+    Router.push('/your')
+  }
+
+  return (
+    <div>
+      <h1>我的</h1>
+      <Button type="primary" onClick={handleClick}>去非Tabbar页面</Button>
+    </div>
+  )
+}
+
+export default Me
