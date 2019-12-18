ae5c3a876a33a547fd49359fb2ccc1a095e44635
Z-HNAN <zhn68033@163.com>
2019-12-18 23:31:04 +0800

    feat 修改路由界面

--- config/router.config.ts
+++ config/router.config.ts
@@ -2,19 +2,15 @@ import { IRoute } from 'umi-types'
 
 /* 项目路由配置 (采用配置式路由) */
 const routes: IRoute[] = [
-  // 默认跳转去post页面 
-  {
-    path: '/',
-    redirect: '/post',
-  },
+  // 默认跳转去post页面
   {
     path: '/',
     component: '../layouts/BasicLayout',
     routes: [
-      { path: '/post', component: './post', title: '岗位' },
-      { path: '/schedule', component: './schedule', title: '计划' },
-      { path: '/me', component: './me', title: '我的' },
+      { path: '/', redirect: '/users' },
       { path: '/users', component: './users', title: '用户' },
+      { path: '/me', component: './me', title: '我的' },
+      { path: '/your', component: './your', title: '你的' },
       { component: '404', title: '页面走丢了...' },
     ],
   }
