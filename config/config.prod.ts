61511f30600ca7c2fe021f97b778e074b2ac580e
Z-HNAN <zhn68033@163.com>
2019-12-19 15:40:22 +0800

    fix eslint代码规范

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -3,7 +3,7 @@ import { IConfig } from 'umi-types'
 const config: IConfig = {
   define: {
     'process.env.base_url': 'http://jsonplaceholder.typicode.com/',
-  }
+  },
 }
 
 export default config
